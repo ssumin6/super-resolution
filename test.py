415cee2d549fb7fdb22e93ddddae793c66e60fe7
ssumin6 <ssumin6@gmail.com>
2020-01-20 04:37:25 -0500

    add bilinear upsample psnr

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 import tensorflow as tf
+import cv2
 
 from model import evaluate, resolve_single
 from model.edsr import edsr
@@ -21,6 +22,18 @@ def restore(checkpoint, checkpoint_manager):
 		checkpoint.restore(checkpoint_manager.latest_checkpoint)
 		print('Model restored from checkpoint at step %d.' %checkpoint.step.numpy())
 
+def bilinear_upscale(valid_ds, scale):
+	psnr_values = []
+    ssim_values = []
+    for lr, hr in dataset:
+        #bilinear upscale
+        sr = cv2.resize(lr, dsize=(lr.shape[0]*scale,lr.shape[1]*scale), interpolation=cv2.INTER_LINEAR)
+        psnr_value = psnr(hr, sr)[0]
+        ssim_value = ssim(hr, sr)[0]
+        psnr_values.append(psnr_value)
+        ssim_values.append(ssim_value)
+    return tf.reduce_mean(psnr_values), tf.reduce_mean(ssim_values)
+
 
 def test():
 	model = edsr(scale = scale, num_res_blocks= depth, num_filters= channels)
@@ -38,6 +51,8 @@ def test():
 
 	psnr, ssim = evaluate(checkpoint.model, valid_ds)
 	print('PSNR:%.3f, SSIM:%.3f' % (psnr, ssim))
+	psnr_b, ssim_b = bilinear_upscale(valid_ds, scale=scale)
+	print('bilinear upscale PSNR:%.3f, SSIM:%.3f' % (psnr_b, ssim_b))
 
 	lr = load_image('../image_240/frame1500.jpg')
 	sr = resolve_single(checkpoint.model, lr)
